51462048d57ff0b74a58454657c25bf20764863d
Víctor Colombo <victorcora98@gmail.com>
2020-07-25 11:44:23 -0300

    Implement multiple new methods

--- src/com/github/vccolombo/singlylinkedlist/SinglyLinkedList.java
+++ src/com/github/vccolombo/singlylinkedlist/SinglyLinkedList.java
@@ -14,6 +14,26 @@ public class SinglyLinkedList<T> {
             this.data = data;
             this.next = null;
         }
+
+        public SinglyLinkedListNode(T data, SinglyLinkedListNode next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    private SinglyLinkedListNode getNodeByIndex(int index) {
+        SinglyLinkedListNode result = this.head;
+        while(index > 0 && result != null) {
+            result = result.next;
+            index--;
+        }
+
+        return result;
+    }
+
+    // Check if Linked List is empty
+    public boolean isEmpty() {
+        return this.size == 0;
     }
 
     // Get length of Linked List
@@ -23,12 +43,11 @@ public class SinglyLinkedList<T> {
 
     // Get element at the index position
     public T get(int index) {
-        SinglyLinkedListNode result = this.head;
-        while(index > 0 && result != null) {
-            result = result.next;
-            index--;
+        if (index < 0 || index > this.size-1) {
+            throw new IndexOutOfBoundsException("Index must be between 0 and " + (this.size-1) + ". Provided: " + index);
         }
 
+        SinglyLinkedListNode result = this.getNodeByIndex(index);
         if (result == null) {
             return null;
         }
@@ -36,32 +55,65 @@ public class SinglyLinkedList<T> {
         return result.data;
     }
 
-    // Get data from head (first element)
+    // Get data from the first element
     public T get() {
+        if (this.head == null) {
+            return null;
+        }
+
         return this.head.data;
     }
 
-    // Get data from head (first element)
+    // Get data from the first element
     public T getFirst() {
         return get();
     }
 
+    // Get data from the last element
     public T getLast() {
+        if (this.tail == null) {
+            return null;
+        }
+
         return this.tail.data;
     }
 
-    // Append to end
-    public void add(T data) {
-        this.size++;
+    // Add data to an specific position
+    public void add(int index, T data) {
+        if (index < 0 || index > this.size) {
+            throw new IndexOutOfBoundsException("Index must be between 0 and " + this.size + ". Provided: " + index);
+        }
 
+        SinglyLinkedListNode insertAfter = this.getNodeByIndex(index-1);
+        insertAfter.next = new SinglyLinkedListNode(data, insertAfter.next);
+        this.size++;
+    }
+
+    // Append to the end
+    public void add(T data) {
         SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
         if (this.head == null) {
             this.head = newNode;
             this.tail = this.head;
-            return;
+        } else {
+            this.tail.next = newNode;
+            this.tail = this.tail.next;
         }
 
-        this.tail.next = newNode;
-        this.tail = this.tail.next;
+        this.size++;
+    }
+
+    // Insert in the beginning
+    public void push(T data) {
+        head = new SinglyLinkedListNode(data, head);
+        this.size++;
+    }
+
+    public void set(int index, T data) {
+        if (index < 0 || index > this.size-1) {
+            throw new IndexOutOfBoundsException("Index must be between 0 and " + (this.size-1) + ". Provided: " + index);
+        }
+
+        this.getNodeByIndex(index).data = data;
     }
 }
--- src/com/github/vccolombo/singlylinkedlist/SinglyLinkedListTest.java
+++ src/com/github/vccolombo/singlylinkedlist/SinglyLinkedListTest.java
@@ -47,10 +47,126 @@ class SinglyLinkedListTest {
     public void addMultipleElementsAndCheckTheSize() {
         SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<>();
         linkedList.add(1);
-        linkedList.add(2);
+        linkedList.push(2);
+        linkedList.add(3);
+        linkedList.add(1, 4);
+
+        assertEquals(4, linkedList.size());
+    }
+
+    @Test
+    public void addElementAtSpecificIndex() {
+        SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<>();
+        linkedList.add(0);
+        linkedList.add(1);
         linkedList.add(3);
         linkedList.add(4);
 
-        assertEquals(4, linkedList.size());
+        linkedList.add(2, 2);
+
+        assertEquals(2, linkedList.get(2));
+    }
+
+    @Test
+    public void throwOutOfBoundsExceptionIfAddIndexIsNegative() {
+        SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<>();
+        linkedList.add(42);
+
+        IndexOutOfBoundsException e = assertThrows(
+                IndexOutOfBoundsException.class, () -> linkedList.add(-1, 27));
+
+        assertEquals("Index must be between 0 and 1. Provided: -1", e.getMessage());
+    }
+
+    @Test
+    public void throwOutOfBoundsExceptionIfAddIndexIsGreaterThanSize() {
+        SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<>();
+        linkedList.add(42);
+
+        IndexOutOfBoundsException e = assertThrows(
+                IndexOutOfBoundsException.class, () -> linkedList.add(3, 27));
+
+        assertEquals("Index must be between 0 and 1. Provided: 3", e.getMessage());
+    }
+
+    @Test
+    public void getMustReturnNullIfEmpty() {
+        SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<>();
+
+        assertNull(linkedList.get());
+    }
+
+    @Test
+    public void getFirstMustReturnNullIfEmpty() {
+        SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<>();
+
+        assertNull(linkedList.getFirst());
+    }
+
+    @Test
+    public void getLastMustReturnNullIfEmpty() {
+        SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<>();
+
+        assertNull(linkedList.getLast());
+    }
+
+    @Test
+    public void isEmptyMustBeTrueIfEmpty() {
+        SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<>();
+
+        assertTrue(linkedList.isEmpty());
+    }
+
+    @Test
+    public void isEmptyMustBeFalseIfNotEmpty() {
+        SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<>();
+        linkedList.add(42);
+
+        assertFalse(linkedList.isEmpty());
+    }
+
+    @Test
+    public void pushToBeginningAndGetFirstElement() {
+        SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<>();
+        linkedList.push(2);
+        linkedList.push(1);
+        linkedList.push(0);
+
+        assertEquals(0, linkedList.getFirst());
+    }
+
+    @Test
+    public void pushToBeginningAndGetSecondElement() {
+        SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<>();
+        linkedList.push(2);
+        linkedList.push(1);
+        linkedList.push(0);
+
+        assertEquals(1, linkedList.get(1));
+    }
+
+    @Test
+    public void replaceAnElement() {
+        SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<>();
+        linkedList.add(0);
+        linkedList.add(42);
+        linkedList.add(2);
+
+        linkedList.set(1, 1);
+
+        assertEquals(1, linkedList.get(1));
+    }
+
+    @Test
+    public void TryToReplaceAnElementOutOfBounds() {
+        SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<>();
+        linkedList.add(0);
+        linkedList.add(42);
+        linkedList.add(2);
+
+        IndexOutOfBoundsException e = assertThrows(
+                IndexOutOfBoundsException.class, () -> linkedList.set(27, 1));
+
+        assertEquals("Index must be between 0 and 2. Provided: 27", e.getMessage());
     }
 }
